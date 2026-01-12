eeed36cd94658d75678a048dc49dc3f09d5d3939
V Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2026-01-12 16:24:28 +0530

    Create missing_numberin_array.cpp

---
+++ missing_numberin_array.cpp
@@ -0,0 +1,14 @@
+// using the total sum approach 0(n) , o(1)
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = n*(n+1)/2;
+        for (int i =0; i < n; i++ ) {
+            sum -= nums[i];
+        }
+        return sum;
+        
+        }
+};
