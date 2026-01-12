589519eccf86e0451e70c422360acdddffe0b9e5
V Bhavesh <143629789+bhavesh2327@users.noreply.github.com>
2026-01-12 16:30:44 +0530

    Update missing_numberin_array.cpp

--- missing_numberin_array.cpp
+++ missing_numberin_array.cpp
@@ -12,3 +12,17 @@ public:
         
         }
 };
+
+//By uisng the xor approach  
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int result = n;
+        for(int i=0; i<n; i++){
+            result ^= i;
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
